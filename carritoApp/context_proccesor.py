f3880dfefe05307fe8876c1564eaaffb67c26782
Wilmar Galvis <wagalvis24@gmail.com>
2025-12-13 22:02:40 -0500

    continuacion configs carritoApp

---
+++ carritoApp/context_proccesor.py
@@ -0,0 +1,9 @@
+def importe_total_carrito(request):
+
+    total = 0
+    if request.user.is_authenticated:
+        for key, value in request.session["Carrito"].items():
+            total = total + (float(value["precio"])*value["cantidad"])
+    return {"importe_total_carrito": total}
+
+
